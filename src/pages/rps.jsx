9a60cf78a5d40ad25a7251722887b212634d9f0c
SayaSukaMartabak <101211042meng@gmail.com>
2022-09-05 13:03:08 +0700

    update view game rules

--- src/pages/rps.jsx
+++ src/pages/rps.jsx
@@ -3,23 +3,24 @@ import Nav from "../components/Nav";
 import Handler from "../components/Handler";
 import PdfFile from "../components/gameRules";
 import { PDFDownloadLink } from "@react-pdf/renderer";
-import { Viewer } from "@react-pdf-viewer/core";
-// import { defaultLayoutPlugin } from "@react-pdf-viewer/default-layout";
-// import "@react-pdf-viewer/core/lib/styles/index.css";
-// import "@react-pdf-viewer/default-layout/lib/styles/index.css";
-
-// const defaultLayoutPluginInstance = defaultLayoutPlugin();
+import Link from "next/link";
 
 export default function Rps() {
   return (
     <>
       <Container className="mt-5">
         <Nav />
-
         <div className="text-center pt-5">
           <h5>Game : Rock Paper Scissors</h5>
         </div>
-
+        <div>
+          <Link href="/ruleSet">
+            <a target="_blank">
+              <button>View Rules Here</button>
+            </a>
+          </Link>
+        </div>
+        <p>OR</p>
         <div>
           <PDFDownloadLink
             document={<PdfFile />}
