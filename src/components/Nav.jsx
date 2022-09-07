cefb44e98a3e548df9e67c8a950404e4f6f8dcd8
brndnart <brndnartt@gmail.com>
2022-09-07 17:44:49 +0700

    fix error eslint

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,27 +3,31 @@ import { Button, Container } from 'react-bootstrap'
 import Link from 'next/link'
 import { logoutInitiate } from '../redux/action'
 
-export default function Nav () {
-  const { logout } = logoutInitiate()
+export default function Nav() {
+    const { logout } = logoutInitiate()
 
-  return (
-    <>
-      <Container className=''>
-        <div className="row">
-          <div className="col-11 pt-1">
-            <Link href="/dashboard" className="link">
-              <h5 style={{ color: 'black' }}>DASHBOARD</h5>
-            </Link>
-          </div>
-          <div className="col pt-1">
-            <Link href="/login">
-              <Button size="sm" variant="primary" onClick={logout}>
-                Logout
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </Container>
-    </>
-  )
+    return (
+        <>
+            <Container className=''>
+                <div className='row'>
+                    <div className='col-11 pt-1'>
+                        <Link href='/dashboard' className='link'>
+                            <h5 style={{ color: 'black' }}>DASHBOARD</h5>
+                        </Link>
+                    </div>
+                    <div className='col pt-1'>
+                        <Link href='/login'>
+                            <Button
+                                size='sm'
+                                variant='primary'
+                                onClick={logout}
+                            >
+                                Logout
+                            </Button>
+                        </Link>
+                    </div>
+                </div>
+            </Container>
+        </>
+    )
 }
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,16 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/index.css";
-import { Auth } from "../context/Auth";
-import { Provider } from "react-redux";
-import { store } from "../redux/store"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/index.css'
+import { Auth } from '../context/Auth'
+import { Provider } from 'react-redux'
+import { store } from '../redux/store'
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider store={store}>
-      <Auth>
-      <Component {...pageProps} />;
-    </Auth>
-    </Provider>
-    
-  );
+    return (
+        <Provider store={store}>
+            <Auth>
+                <Component {...pageProps} />;
+            </Auth>
+        </Provider>
+    )
 }
 
-export default MyApp;
+export default MyApp
